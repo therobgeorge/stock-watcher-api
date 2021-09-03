efe109ede621c265e2b1df9147943a531b88b6fb
therobgeorge <georg1rd@gmail.com>
2021-09-02 22:08:44 -0400

    adds ransack gem

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,9 @@
 class StocksController < ApplicationController
   def index
-    stocks = Stock.where("name LIKE ?", "%" + params[:q] + "%")
-    render json: stocks.as_json
+    # stocks = Stock.all
+    @q = Stock.ransack({ name_i_cont: "apple" })
+    @stock = @q.result(distinct: true)
+    binding.pry
+    render @stock
   end
 end
